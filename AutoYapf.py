cd49ab281f5af833a1d9e5d95499d460274c6ba9
Marc Brinkmann <git@marcbrinkmann.de>
2015-08-08 18:28:44 +0200

    Use stdin/out/err instead of temporary files.

--- AutoYapf.py
+++ AutoYapf.py
@@ -1,26 +1,8 @@
-from contextlib import contextmanager
 import os
 import subprocess
-import tempfile
 
 import sublime
 import sublime_plugin
-
-
-@contextmanager
-def temporary_file(*args, **kwargs):
-    ignore_unlinked = kwargs.pop('ignore_unliked', False)
-
-    fp = tempfile.NamedTemporaryFile(*args, delete=False, **kwargs)
-    try:
-        yield fp
-    finally:
-        try:
-            os.unlink(fp.name)
-        except OSError as e:
-            # if the file does not exist anymore, ignore
-            if e.errno != 2 or ignore_unlinked is False:
-                raise
 
 
 class EventListener(sublime_plugin.EventListener):
@@ -38,28 +20,27 @@ class AutoYapfCommand(sublime_plugin.TextCommand):
         bytes = self.view.substr(selection).encode('utf-8')
 
         # dump source into temporary file
-        with temporary_file(suffix='.py') as fp:
-            name = fp.name
-            fp.write(bytes)
-            fp.close()
+        args = ['yapf', '--verify']
+        env = os.environ.copy()
+        env['LANG'] = 'utf-8'
 
-            # run yapf
-            args = ['yapf', '--verify', '--in-place', name]
-            env = os.environ.copy()
-            env['LANG'] = 'utf-8'
-            popen = subprocess.Popen(args, env=env, stderr=subprocess.PIPE)
-            _, stderr = popen.communicate()
-            if popen.returncode:
-                error_lines = stderr.decode('utf-8').strip().replace(
-                    '\r\n', '\n').split('\n')
-                loc, msg = error_lines[-4], error_lines[-1]
-                loc = loc[loc.find('line'):]
-                sublime.status_message('yapf: %s @ %s' % (msg, loc))
-                return
+        popen = subprocess.Popen(
+            args,
+            env=env,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE)
+        yapf_result, stderr = popen.communicate(bytes)
 
-            # read back in
-            new_text = open(name, 'rb').read().decode('utf-8').replace('\r\n',
-                                                                       '\n')
+        if popen.returncode:
+            error_lines = stderr.decode('utf-8').strip().replace(
+                '\r\n', '\n').split('\n')
+            loc, msg = error_lines[-4], error_lines[-1]
+            loc = loc[loc.find('line'):]
+            sublime.status_message('yapf: %s @ %s' % (msg, loc))
+            return
+
+        new_text = yapf_result.decode('utf-8').replace('\r\n', '\n')
 
         # replace it
         self.view.replace(edit, selection, new_text)
